20c87451d7531c5dc2c247996713ffe3d32c7542
dancriddle <dancriddle@users.noreply.github.com>
2014-12-06 23:34:38 +0000

    Changed port to 5011 to avoid clash with JPL microservice

--- service.py
+++ service.py
@@ -30,4 +30,4 @@ def send_mail():
   return "OK"
 
 if __name__ == '__main__':
-  app.run(debug=True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5009)))
+  app.run(debug=True,host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 5011)))
